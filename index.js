d507ee4a49aed70142ef082448367169ff7badc2
amath vaillam <amathv01@gmail.com>
2022-09-22 10:30:56 +0000

    calculator design skeletton

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const data = [
-    { class: "clear", value: "clear", display: "AC", key: "192" },
+let firstOperand = 0
+let secondOperand = 0
+let output = ""
+let operator = ""
+
+const datas = [{
+        class: "clear",
+        value: "clear",
+        display: "AC",
+        key: "192",
+        listenerOnClick: (event) => {
+
+        },
+    },
     { class: "sign", value: "sign", display: "+/-", key: "193" },
     { class: "percent", value: "%", display: "%", key: "194" },
     { class: "operator", value: "/", display: "/", key: "111" },
     { class: "operand", value: "7", display: "7", key: "103" },
-    { class: "operand", value: "8", display: "7", key: "104" },
-    { class: "operand", value: "9", display: "7", key: "105" },
+    { class: "operand", value: "8", display: "8", key: "104" },
+    { class: "operand", value: "9", display: "9", key: "105" },
     { class: "operator", value: "*", display: "X", key: "191" },
     { class: "operand", value: "4", display: "4", key: "100" },
     { class: "operand", value: "5", display: "5", key: "101" },
@@ -18,5 +30,33 @@ const data = [
     { class: "operand", value: "0", display: "0", key: "96" },
     { class: "point", value: ".", display: ".", key: "110" },
     { class: "operator", value: "=", display: "=", key: "187" },
-
 ]
+const init = () => {
+    let container = document.querySelector("#pad")
+    let buttons = datas.map(data => {
+        let button = document.createElement('button')
+        button.classList.add(data.class)
+        button.setAttribute("value", data.value)
+        button.setAttribute("data-key", data.key)
+        button.textContent = data.display
+        return button
+    })
+    let i = 1;
+    let div = document.createElement("DIV")
+    div.classList.add("button-range")
+    for (let button of buttons) {
+        if (i % 5 == 0) {
+            container.appendChild(div)
+            div = document.createElement("DIV")
+            div.classList.add("button-range")
+            i = 1
+        }
+        div.appendChild(button)
+        i++
+    }
+    return buttons
+}
+
+const main = (function() {
+    console.log(init())
+})()
